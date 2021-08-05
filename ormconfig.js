ba3eec6c0f3c7d235cf6798ea84ae255f5a8e784
Willian Silva <silva.williansantos@gmail.com>
2021-08-04 21:02:13 -0300

    Setup project

---
+++ ormconfig.js
@@ -0,0 +1,17 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  cache: {
+    duration: 20000,
+  },
+  entities: [process.env.TYPEORM_ENTITIES_DIR],
+  migrations: [process.env.TYPEORM_MIGRATIONS_DIR],
+  cli: {
+    migrationsDir: 'src/database/migrations',
+    entitiesDir: 'src/models',
+  },
+};
